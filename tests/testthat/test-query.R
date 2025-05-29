a62e024a60e0909eebc8571574f03904aad0edd1
Christopher Kenny <ctk44@cornell.edu>
2025-05-29 18:12:32 -0400

    sometimes list

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -1,5 +1,8 @@
 test_that('typr_query works', {
   skip_if_no_typst()
 
-  expect_true(is.data.frame(typr_query('../test.typ', '<my-sec>')))
+  expect_true(
+    is.data.frame(typr_query('../test.typ', '<my-sec>')) ||
+      is.list(typr_query('../test.typ', '<my-sec>'))
+  )
 })
