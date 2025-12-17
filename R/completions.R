39a48d5ed429b07c24a9f7dc0722c488448ba8b9
Christopher Kenny <ctk44@cornell.edu>
2025-12-17 12:07:09 -0500

    don't break on old versions

--- R/completions.R
+++ R/completions.R
@@ -1,5 +1,8 @@
 #' Retrieve shell completions
 #'
+#' Support for this function was added in Typst 0.14.0. If an earlier version
+#' is in used, an empty character vector is returned.
+#'
 #' @param shell The shell to generate completions for. One of
 #' `c('bash', 'elvish', 'fish', 'powershell', 'zsh')`.
 #'
@@ -10,6 +13,9 @@
 #' typr_completions(shell = 'powershell')
 typr_completions <- function(shell = c('bash', 'elvish', 'fish',
                                        'powershell', 'zsh')) {
+  if (typr_version() < '0.14.0') {
+    return(character())
+  }
   shell <- match.arg(shell)
   out <- typr_run(args = c('completions', shell))$stdout
   strsplit(out, '\n')[[1]]
--- R/info.R
+++ R/info.R
@@ -1,11 +1,17 @@
 #' Display Typst Debugging Information
 #'
+#' Support for this function was added in Typst 0.14.0. If an earlier version
+#' is in used, an empty character vector is returned.
+#'
 #' @returns a character vector of debugging information
 #' @export
 #'
 #' @examplesIf has_any_typst()
 #' typr_info()
 typr_info <- function() {
+  if (typr_version() < '0.14.0') {
+    return(character())
+  }
   out <- typr_run(args = 'info')$stderr
   strsplit(out, '\n')[[1]]
 }
